f02853aec1a45c094278dea5612f4d8832b4b6b8
Ch3mie <ryanc668@nycstudents.net>
2024-10-23 12:06:25 -0400

    e

--- asdad.js
+++ asdad.js
@@ -1,6 +1,15 @@
-DOMSelector.container.insertAdjacentHTML(
-  "beforeend",
-  <div>
-    <p>HEll OWrld</p>
-  </div>
-);
+function parking(x,y) {
+  let x = 
+  let y = 
+}
+
+
+//add a yesterday and today
+
+//Make the total amount of parking spaces 100
+
+//allow cars to park in any space
+
+//make some cars disappear from certain spaces but also add new cars to empty spaces
+
+//calculate how many oringinal cars are left
