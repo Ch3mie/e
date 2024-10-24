0cedc1430750615fd2cdfd8ce825e5897c2b3f15
Ch3mie <ryanc668@nycstudents.net>
2024-10-24 12:27:33 -0400

    e

---
+++ decimaltobinary.js
@@ -0,0 +1,14 @@
+function change(binary) {
+  //let the decimal = the length of the binary
+  //binary multiply each digit by 2^n
+  //let n start at 0
+  //add one to n from right to left
+  //multiply 2^n by the digits of the binary
+
+let decimal = 0;
+let length = binary.length
+
+  for (let n = 0; n < length; n++) {
+    if()
+  }
+}
