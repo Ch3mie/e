09de5b2c79b190eb946ea142c544cb5e76255b96
Ch3mie <ryanc668@nycstudents.net>
2024-10-25 12:27:04 -0400

    e

--- decimaltobinary.js
+++ decimaltobinary.js
@@ -5,10 +5,27 @@ function change(binary) {
   //add one to n from right to left
   //multiply 2^n by the digits of the binary
 
-let decimal = 0;
-let length = binary.length
+  let decimal = 0;
+  let length = binary.length;
 
   for (let n = 0; n < length; n++) {
-    if()
+    if (binary[binary.length - 1 - n] === "1") {
+      decimal = decimal + Math.pow(2, n);
+      // decimal = decimal + 2^0
+    }
+  }
+  return decimal;
+}
+
+console.log(change("1110"));
+
+function dectobin(decimal) {
+  let binary = "";
+  while (decimal != 0) {
+    if (decimal % 2 == 1) {
+      binary += "1";
+    } else {
+      binary += "0";
+    }
   }
 }
